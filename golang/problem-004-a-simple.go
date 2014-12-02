a662643bb5acb7f8a396adf36fbcf9b23f0e7bd0
Patrick Byrne <code@patrickbyrne.net>
2014-12-02 07:31:20 -0600

    WIP problem 4

---
+++ golang/problem-004-a-simple.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+  "fmt"
+  "strings"
+)
+
+func main() {
+  palindromes := make([]int, 0)
+
+  for i := 999; i > 0; i-- {
+    for j := 999; j > 0; j-- {
+      product := i * j
+      if isPalindromeNumber(product) {
+        palindromes = append(palindromes, product)
+      }
+    }
+  }
+
+  product := palindromes[0]
+
+  fmt.Printf("The largest palindrome product of two 3-digit numbers is %v", product)
+}
+
+func isPalindromeNumber(value int) bool {
+  return string(value) == reversedString(value)
+}
+
+func reversedString(value int) string {
+  stringy := string(value)
+  length := len(stringy)
+  result := make([]string, length)
+  for i, c := range stringy {
+    fmt.Printf("Reversing %v (length %v, index %v, fetching %v)", stringy, i, length-i)
+    result[length-i] = string(c)
+  }
+  return strings.Join(result, "")
+}
